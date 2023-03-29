cf0fe665ee56e8eedd9b6aae7c36c9ab6732627f
Alexandre Delbarre <alexandre.delbarre@hotmail.fr>
2023-03-29 11:58:42 +0200

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import styles from "./App.css";
 
 function App() {
   return (
-    <div className="Bulbizar">
-      <PokemonCard />
-    </div>
+    <PokemonCard props={pokemonList[0]} />
   );
 }
+
+const pokemonList = [
+  {
+    name: "Bulbizar",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  }
+];
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,8 @@
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard({ props }) {
+    const pokemon = props;
     return (
         pokemon.imgSrc ? <figure><img src={pokemon.imgSrc} /> <figcaption>{pokemon.name}</figcaption></figure> : <figcaption>{pokemon.name} <p>???</p></figcaption>);
 }
-
-const pokemonList = [
-    {
-        name: "Bulbizar",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "Mew",
-    }
-];
 
 
 
